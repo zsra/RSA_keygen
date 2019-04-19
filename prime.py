0e57c042ae3d45a7bba517f63a3b47bd31576fe8
zsra <45008532+zsra@users.noreply.github.com>
2019-04-19 14:50:59 +0200

    Update prime.py

--- prime.py
+++ prime.py
@@ -6,22 +6,27 @@ def GetPrime(min, max):
     primes = [i for i in range(min, max) if isPrime(i)]
     return random.choice(primes)
 
-def isPrime(n):
-    if n == 2:
-        return True
-    if n % 2 == 0 or n <= 1:
-        return False
+def rabinMiller(num):
+    # Returns True if num is a prime number.
 
-    sqr = int(math.sqrt(n)) + 1
+    s = num - 1
+    t = 0
+    while s % 2 == 0:
+        # keep halving s while it is even (and use t
+        # to count how many times we halve s)
+        s = s // 2
+        t += 1
 
-    for divisor in range(3, sqr, 2):
-        if n % divisor == 0:
-            return False
+    for trials in range(5): # try to falsify num's primality 5 times
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1: # this test does not apply if v is 1.
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
     return True
-
-def isCoPrime(a, b):
-    if(xgcd.egcd(a, b)[0] == 1):
-        return True
-    else:
-        return False
 
